97ae5a6f27678839d514a37f4bbc3b4ab38b23ef
Johnny Deymisson <jdeymisson@gmail.com>
2023-07-17 21:05:05 -0300

    :sparkles: feat: Subindo modal confirmAction

---
+++ src/components/ConfirmAction/index.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useRef } from "react";
+import { ButtonComponent } from "../Button";
+import { Container, ContentConfirmAction } from "./styles";
+
+export const ConfirmAction = ({ text, handleClickCofirm, handleClickCancel, openModalActionConfirm }) => {
+    const innerConfirmRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (innerConfirmRef.current && !innerConfirmRef.current.contains(event.target)) {
+                handleClickCancel();
+            };
+        };
+    
+        const handleKeyPress = (event) => {
+            if (event.key === 'Escape') {
+                handleClickCancel();
+            };
+        };
+    
+        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('keydown', handleKeyPress);
+    
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
+
+    return(
+        <>
+        {openModalActionConfirm ?
+            <Container>
+                <ContentConfirmAction ref={innerConfirmRef}>
+                    <p>{text}</p>
+                    <div className="actions-buttons">
+                        <ButtonComponent
+                            title="Cancelar" 
+                            color="BLACK"
+                            handleClick={handleClickCancel}
+                        />
+                        <ButtonComponent 
+                            title="Confirmar" 
+                            color="YELLOW"
+                            handleClick={handleClickCofirm}
+                        />
+                    </div>
+                </ContentConfirmAction>
+            </Container>
+            : null
+        }
+        </>
+    );
+};
---
+++ src/components/ConfirmAction/styles.js
@@ -0,0 +1,44 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    right: 0;
+    background: rgba(0, 0, 0, .5);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 1;
+`;
+
+export const ContentConfirmAction = styled.div`
+    width: 32rem;
+    height: 24rem;
+
+    background: ${({ theme }) => theme.COLORS.RED};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+    border-radius: .6rem;
+    padding: 1.6rem;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    > p {
+        margin-top: 2.4rem;
+        font-size: 1.6rem;
+        text-align: center;
+        font-weight: 700;
+    }
+
+    > .actions-buttons {
+        display: flex;
+        gap: .8rem;
+
+        width: 100%;
+    }
+`;
