2e847ba1633c03e9d7491ca15e1892f4ebeab9a3
Johnny Deymisson <jdeymisson@gmail.com>
2023-07-18 19:57:15 -0300

    :sparkles: feat: Finalizando a interface de SignUp, integrando com o backend

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,60 @@
+import { useState } from "react";
 import { Container, Form, Logo } from "./styles";
-import logo from "../../assets/logo-bombeiros.png"
-import { InputComponent } from "../../components/Input";
 import { ButtonComponent } from "../../components/Button";
+import { InputComponent } from "../../components/Input";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo-bombeiros.png"
+import { api } from "../../services/api";
+import { isValidCPF, isValidEmail } from "../../utils/fieldValidation";
 
 export const SignUp = () => {
+    const [cpf, setCpf] = useState("");
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const registerUsers = async (event) => {
+        event.preventDefault();
+        
+        const cpfIsValid = isValidCPF(cpf);
+        const emailIsValid = isValidEmail(email);
+
+        if(!cpfIsValid){
+            return alert("Para se cadastrar é necessário preencher um CPF válido!");
+        };
+
+        if(!emailIsValid){
+            return alert("Para se cadastrar é necessário informar um e-mail válido!");
+        };
+
+        if(password.length < 6){
+            return alert("Para se cadastrar é necessário informar uma senha com no minímo 6 caracter!");
+        };
+
+        setLoading(prevState => !prevState);
+
+        const dados = {
+            cpf,
+            name,
+            email,
+            password
+        };
+
+        try{
+            const response = await api.post("/users", dados);
+            if(response.status === 201) {
+                alert(response.data.message);
+            };
+            
+        } catch(error) {
+            if(error.response.data) {
+                alert(error.response.data.message);
+                setLoading(false);
+            };
+        };
+    };
+
     return (
         <Container>
             <Logo>
@@ -19,31 +70,37 @@ export const SignUp = () => {
                         type="text"
                         placeholder="Ex: 111.222.333-75"
                         title="CPF"
+                        onChange={({target}) => setCpf(target.value)}
                     />
                     <InputComponent
                         id="name"
                         type="text"
                         placeholder="Ex: Johnny Deymisson"
                         title="Nome"
+                        onChange={({target}) => setName(target.value)}
                     />
                     <InputComponent
                         id="email"
                         type="text"
                         placeholder="Ex: bombeiros@gmail.com"
                         title="Email"
+                        onChange={({target}) => setEmail(target.value)}
                     />
                     <InputComponent
                         id="password"
                         type="password"
                         placeholder="********"
                         title="Senha"
+                        onChange={({target}) => setPassword(target.value)}
                     />
 
                     <ButtonComponent 
-                        title="Entrar"
+                        title="Cadastrar"
                         color="YELLOW"
+                        onClick={registerUsers}
+                        loading={loading}
                     />
-                    <a>Crie sua conta</a>
+                    <Link to="/">Faça Login</Link>
                 </form>
             </Form>
         </Container>
