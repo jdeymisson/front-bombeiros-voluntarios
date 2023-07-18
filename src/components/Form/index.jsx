d05fecaddfb10cdef33f2db9d92ecd966846d537
Johnny Deymisson <jdeymisson@gmail.com>
2023-07-17 21:11:35 -0300

    :sparkles: feat: Subindo compoment form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,37 @@
-import { Container } from "./styles";
+import { useEffect, useRef } from "react";
+import { Container, Form } from "./styles";
 
-export const FormComponent = ({ children, innerFormRef }) => {
+export const FormComponent = ({ children, setModalProfileOpen }) => {
+    const innerFormRef = useRef();
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+          if (innerFormRef.current && !innerFormRef.current.contains(event.target)) {
+            setModalProfileOpen(false);
+          };
+        };
+    
+        const handleKeyPress = (event) => {
+          if (event.key === 'Escape') {
+            setModalProfileOpen(false);
+          };
+        };
+    
+        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('keydown', handleKeyPress);
+    
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+          document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
+
     return(
-        <Container ref={innerFormRef}>
-            {children}
+        <Container>
+            <Form 
+              ref={innerFormRef}>
+                {children}
+            </Form>
         </Container>
-    )
+    );
 };
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,6 +1,16 @@
 import { styled } from "styled-components";
 
-export const Container = styled.form`
+export const Container = styled.div`
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        right: 0;
+        background: rgba(0, 0, 0, .5);
+        z-index: 1;
+`;
+
+export const Form = styled.form`
     position: relative;
     top: 50%;
     left: 50%;
