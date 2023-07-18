cc255891691a6d71649691eacc93d4ca2e37f415
Johnny Deymisson <jdeymisson@gmail.com>
2023-07-17 21:11:56 -0300

    :sparkles: feat: Subindo compoment header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,25 +2,17 @@ import { styled } from "styled-components";
 
 export const Container = styled.header`
     grid-area: header;
-    
     height: 100%;
     width: 100%;
     padding: 0 3.2rem;
+    top: 0;
+    left: 0;
 
     border-bottom: 1px solid ${({ theme }) => theme.COLORS.GRAY_300};
-    
+    background: ${({ theme }) => theme.COLORS.WHITE};
     display: flex;
     justify-content: flex-end;
     align-items: center;
-
-    > .filter {
-        position: absolute;
-        height: 100%;
-        width: 100%;
-        top: 0;
-        right: 0;
-        background: rgba(0, 0, 0, .5);
-    }
 `; 
 
 export const Profile = styled.div`
